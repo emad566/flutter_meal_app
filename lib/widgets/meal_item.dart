2f76dd8e0a33fc09765558a4b8ec654d0dcad576
emad566 <emade09@gmail.com>
2023-04-03 10:16:14 +0200

    Finish meal item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meal_app/models/meal.dart';
 import 'package:flutter_meal_app/shared/components.dart';
@@ -7,6 +6,24 @@ import 'package:flutter_meal_app/shared/components.dart';
 class MealItem extends StatelessWidget {
   Meal item;
   MealItem({Key key, @required this.item}) : super(key: key);
+
+  String get complexityText{
+    switch(item.complexity){
+      case Complexity.simple: return 'Simple'; break;
+      case Complexity.challenging: return 'Challenging'; break;
+      case Complexity.hard: return 'Hard'; break;
+      default: return 'Unknown'; break;
+    }
+  }
+
+  String get affordability{
+    switch(item.affordability){
+      case Affordability.affordable: return 'Affordable'; break;
+      case Affordability.luxurious: return 'Luxurious'; break;
+      case Affordability.pricey: return 'Pricey'; break;
+      default: return 'Unknown'; break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,17 +56,46 @@ class MealItem extends StatelessWidget {
                   ),
                 ),
                 Positioned(
-                  child: Container(
-                    width: queryData.size.width -40,
-                    padding: EdgeInsets.all(20),
-                    color: Colors.black.withOpacity(.5),
-                    child: myText(item.title, style: TextStyle(color: Colors.white)),
-                  ),
                   bottom: 20,
                   left: 20,
+                  child: Container(
+                    width: queryData.size.width -40,
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.black.withOpacity(.5),
+                    child: myText(item.title, style: const TextStyle(color: Colors.white)),
+                  ),
                 )
               ],
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(width: 6,),
+                      Text('${item.duration} min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(width: 6,),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 6,),
+                      Text(affordability),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
